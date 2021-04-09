ae010367f1bc90c6d6b8c02a579f1bc9375066d7
Harry Fox <hfox@protonmail.com>
2021-04-08 19:39:09 -0500

    Created basic components

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,46 +1,21 @@
 <template>
   <div id="app">
     <div class="nav">
-      <router-link to="/">Hello Earth</router-link>|<router-link to="/about"
-        >World</router-link
-      >
+      <router-link to="/">Hello Earth</router-link>|<router-link to="/about">World</router-link>
     </div>
     <router-view />
+    <Navbar />
+
   </div>
 </template>
 
-// <style lang="scss">
-// // @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap");
+<script>
+import Navbar from "./components/NavBar.vue";
 
-// // #root {
-// //   --font: Roboto, sans-serif;
-// //   --textColor: #374961;
-// //   --linkActiveColor: #41b783;
-// // }
-
-// #app {
-//   font-family: var(--font);
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: var(--textColor);
-
-//   .logo {
-//     width: 20%;
-//   }
-// }
-
-// .nav {
-//   padding: 30px 0 100px 0;
-
-//   a {
-//     font-weight: 500;
-//     color: var(--textColor);
-//     margin: 0 5px;
-//   }
-
-//   a.router-link-exact-active {
-//     color: var(--linkActiveColor);
-//   }
-// }
-// </style>
+export default {
+  name: "App",
+  components: {
+    Navbar
+  }
+};
+</script>
