e3f2b0d5c6e6b4c6584fa93e83ea82d093483a31
Harry Fox <hfox@protonmail.com>
2021-04-14 12:09:54 -0500

    Got proper graph style for web metrics

--- src/app/components/WebMetrics.vue
+++ src/app/components/WebMetrics.vue
@@ -7,21 +7,89 @@
 export default {
   name: 'WebMetrics',
   data: () => ({
-          series: [70],
+          
+          series: [125],
           chartOptions: {
             chart: {
               height: 350,
               type: 'radialBar',
+              toolbar: {
+                show: true
+              }
             },
             plotOptions: {
               radialBar: {
-                hollow: {
-                  size: '30%',
+                startAngle: -135,
+                endAngle: 225,
+                 hollow: {
+                  margin: 0,
+                  size: '70%',
+                  background: '#fff',
+                  image: undefined,
+                  imageOffsetX: 0,
+                  imageOffsetY: 0,
+                  position: 'front',
+                  dropShadow: {
+                    enabled: true,
+                    top: 3,
+                    left: 0,
+                    blur: 4,
+                    opacity: 0.24
+                  }
+                },
+                track: {
+                  background: '#fff',
+                  strokeWidth: '67%',
+                  margin: 0, // margin is in pixels
+                  dropShadow: {
+                    enabled: true,
+                    top: -3,
+                    left: 0,
+                    blur: 4,
+                    opacity: 0.35
+                  }
+                },
+            
+                dataLabels: {
+                  show: true,
+                  name: {
+                    offsetY: -10,
+                    show: true,
+                    color: '#888',
+                    fontSize: '17px'
+                  },
+                  value: {
+                    formatter: function(val) {
+                      return parseInt(val);
+                    },
+                    color: '#111',
+                    fontSize: '36px',
+                    show: true,
+                  }
                 }
-              },
+              }
             },
-            labels: ['Cricket'],
+            fill: {
+              colors: ['#e32d4c'],
+              type: 'gradient',
+              gradient: {
+                shade: 'dark',
+                type: 'horizontal',
+                shadeIntensity: 0.5,
+                gradientToColors: ['#ABE5A1'],
+                inverseColors: true,
+                opacityFrom: 1,
+                opacityTo: 1,
+                stops: [0, 100],
+              }
+            },
+            stroke: {
+              lineCap: 'round'
+            },
+            labels: ['Percent'],
           },
+          
+          
         })
 }
 </script>
