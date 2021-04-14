c538612b70ab6ceabaee9896a1eaf606652a74de
Harry Fox <hfox@protonmail.com>
2021-04-14 17:46:26 -0500

    Expanded Web Metric Graphs

--- src/app/containers/MainContainer.vue
+++ src/app/containers/MainContainer.vue
@@ -3,8 +3,11 @@
     <p>I am main container component</p>
       <Map v-if="isMapView" :treex="treex" />
     <div v-else class="webMetricContainer">
-      <web-metrics name="LCP"/>
-      <web-metrics name="SFX"/>
+      <web-metrics webMetricName="LCP" :webMetricValue="LCP"/>
+      <web-metrics webMetricName="TTFB" :webMetricValue="TTFB"/>
+      <web-metrics webMetricName="FID" :webMetricValue="FID"/>
+      <web-metrics webMetricName="FCP" :webMetricValue="FCP"/>
+      <web-metrics webMetricName="CLS" :webMetricValue="CLS"/>
     </div>
     
   </div>
@@ -20,6 +23,15 @@ export default {
   props: {
     isMapView: Boolean,
     treex: Object
+  },
+  data(){
+    return {
+      LCP: localStorage.LCP,
+      TTFB: localStorage.TTFB,
+      FID: localStorage.FID,
+      FCP: localStorage.FCP,
+      CLS: localStorage.CLS,
+    }
   }
 }
 </script>
